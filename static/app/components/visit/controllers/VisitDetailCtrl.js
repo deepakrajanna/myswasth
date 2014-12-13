778996ccf9be5cf00cfc80a1eac533b325ef2360
Anant Mittal <mittal.anant@gmail.com>
2014-12-13 14:04:10 +0530

    Fixed Visit Page

--- static/app/components/visit/controllers/VisitDetailCtrl.js
+++ static/app/components/visit/controllers/VisitDetailCtrl.js
@@ -2,14 +2,20 @@
 visitControllers.controller('VisitDetailCtrl', ['$scope', '$routeParams', '$http','Visit','$rootScope',
    function ($scope, $routeParams, $http, Visit, $rootScope) {
 	
-	alert($routeParams.visitId);
-	alert($rootScope.selected.name);
 	var visit_id = $routeParams.visitId;
 	
-	var responseVisit = Visit.query({visitId: visit_id});
+	var patient_id = $rootScope.selected.id;
+	
+	/*
+	var responseVisit = Visit.query({patientId: patient_id, visitId: visit_id});
     
     responseVisit.$promise.then(function(data){
 	    var VisitData = angular.fromJson(angular.toJson(data)); 
 	    $scope.visit = VisitData;
 	});
+    */
+	
+    Visit.query({ patientId: patient_id, visitId: visit_id }, function(data) {
+    	$scope.visit = data;
+    });
 }]);
