930f57ee6cc6ae794a9594577b855708d298216c
Anant Mittal <mittal.anant@gmail.com>
2015-01-10 14:44:18 +0530

    Hm

--- static/app/components/visit/controllers/VisitDetailCtrl.js
+++ static/app/components/visit/controllers/VisitDetailCtrl.js
@@ -6,22 +6,7 @@ visitControllers.controller('VisitDetailCtrl', ['$scope', '$routeParams', '$http
 	
 	var patient_id = $rootScope.selected.id;
 	
-	/*
-	var responseVisit = Visit.query({patientId: patient_id, visitId: visit_id});
-    
-    responseVisit.$promise.then(function(data){
-	    var VisitData = angular.fromJson(angular.toJson(data)); 
-	    $scope.visit = VisitData;
-	});
-    */
-	/*
-    Visit.query({ patientId: patient_id, visitId: visit_id }, function(data) {
-    	$scope.visit = data;
-    }, function(response) {
-    console.log(response);
-    });
-    */
-    Visit.query({ patientId: patient_id, visitId: visit_id },function(data) {
+	Visit.query({ patientId: patient_id, visitId: visit_id },function(data) {
     	console.log(data);
     	$scope.visit = data;
     }, function(error) {
